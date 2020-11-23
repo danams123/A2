55feefc64b9398b3d6e4b50bd666ffbaf6b75518
danam <dan.am5555555@gmail.com>
2020-11-24 01:19:54 +0200

    Shani's update on Ewoks

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -14,5 +14,18 @@ import java.util.List;
  * You can add ONLY private methods and fields to this class.
  */
 public class Ewoks {
+    private ArrayList<Ewok> EwoksList;
+    private int size;
 
+    public Ewoks(int EwokNum) {
+        EwoksList = new ArrayList<>(EwokNum);
+        for (int i = 1; i <= EwokNum; i++) {
+            EwoksList.add(new Ewok(i));
+        }
+        size = EwokNum;
+    }
+
+    public int size() {return size;}
+
+    public ArrayList<Ewok> getEwoksList() {return this.EwoksList;}
 }
