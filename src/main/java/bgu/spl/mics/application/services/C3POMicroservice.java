c41f2eb2d8de066c9a1e64f974dc9734958b899a
danam <dan.am5555555@gmail.com>
2020-11-30 00:11:14 +0200

    deletef BDcallback because we had a class already

--- src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
@@ -1,10 +1,7 @@
 package bgu.spl.mics.application.services;
 import java.util.List;
 import bgu.spl.mics.MicroService;
-import bgu.spl.mics.application.messages.AttackEvent;
-import bgu.spl.mics.application.messages.BDEventCallBack;
-import bgu.spl.mics.application.messages.TerminateBroadcast;
-import bgu.spl.mics.application.messages.TerminateCallback;
+import bgu.spl.mics.application.messages.*;
 import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
@@ -26,7 +23,7 @@ public class C3POMicroservice extends MicroService {
 
     @Override
     protected void initialize() {
-        this.subscribeEvent(AttackEvent.class, new BDEventCallBack());
+        this.subscribeEvent(AttackEvent.class, new BombCallback());
         this.subscribeBroadcast(TerminateBroadcast.class, new TerminateCallback());
     }
 }
--- src/test/java/bgu/spl/mics/ExampleEvent.java
+++ src/test/java/bgu/spl/mics/ExampleEvent.java
@@ -1,7 +1,5 @@
 package bgu.spl.mics;
 
-import bgu.spl.mics.Event;
-
 public class ExampleEvent implements Event{
 
     private String senderName;
